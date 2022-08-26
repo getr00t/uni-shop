bb6c833f4f2a7d186b1e63c4fdf025f106491819
r00t <yongyubai@qq.com>
2022-08-26 17:25:36 +0800

    home页面

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 请求库
+import { $http } from '@escook/request-miniprogram'
+// 注册到uni
+uni.$http = $http
 
+// 请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1/'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
+
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		首页
+		<!-- swiper -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="item in swiperList" :key="item.goods_id">
+				<view class="swiper-item">
+					<image :src="item.image_src"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +15,43 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [],
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get("home/swiperdata")
+				if (res.meta.status !== 200) {
+					uni.showToast({
+						title: '数据请求失败',
+						duration: 5000,
+						icon: 'error'
+					});
+
+				} else {
+					this.swiperList = res.message
+				}
+				console.log(res);
+			}
+
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 340rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+
+		}
+	}
 </style>
